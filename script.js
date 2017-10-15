b52da4d8940930bc6055ac8e1da000809afa9afa
Aryan Khandal <aryankhandal0@gmail.com>
2017-10-15 09:20:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+/**
+ * Created by ak on 14/10/17.
+ */
+var start = new Date().getTime();
+colors = ["red","black","green","blue","yellow"];
+var color;
+function destColor() {
+    color = Math.floor(Math.random() * 5);
+    document.getElementById("display").innerHTML = "You have to select the color " + colors[color];
+}
+destColor();
+function shapeAppear(){
+    //var width = (Math.random()*200) + 100;
+    //document.getElementById("shape").style.width=width;
+    //document.getElementById("shape").style.height=width;
+    document.getElementById("shape").style.display="block";
+    var color2 = Math.floor(Math.random()*5);
+    document.getElementById("shape").style.backgroundColor=colors[color2].toUpperCase();
+    if(colors[color2]==colors[color]){
+        start = new Date().getTime();
+    } else {
+        changeAfterDelay();
+    }
+}
+
+function changeAfterDelay() {
+
+    setTimeout(shapeAppear, Math.random() * 2000);
+
+}
+changeAfterDelay();
+document.getElementById("shape").onclick = function(){
+    if(document.getElementById("shape").style.backgroundColor==colors[color]){
+        var end = new Date().getTime();
+        var tt = end-start;
+        document.getElementById("timeTaken").innerHTML = (tt/1000)+" seconds";
+        destColor();
+        changeAfterDelay();
+    } else {
+        alert("Wrong Choice");
+        destColor();
+    }
+};
